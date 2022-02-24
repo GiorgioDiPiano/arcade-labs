37e509bb4907f931c9da080bb9ec17e79fa33921
George Dinu <gdinu3786@gmail.com>
2022-02-24 22:39:16 +0100

    Proyecto del lab03 hecho con animacion de las aletas de tiburón

--- lab03-animation/lab03-animation.py
+++ lab03-animation/lab03-animation.py
@@ -3,9 +3,11 @@ import arcade
 def draw_sea():
     arcade.draw_lrtb_rectangle_filled(0, 800, 330, 0, arcade.color.BLUE_SAPPHIRE)
 
+# Draws the beach
 def draw_beach():
     arcade.draw_lrtb_rectangle_filled(0, 800, 230, 0, arcade.color.SAND)
 
+# Draws the red flags
 def draw_flags():
     arcade.draw_rectangle_filled(170, 180, 9, 80, arcade.color.BLACK)
     arcade.draw_rectangle_filled(260, 180, 9, 80, arcade.color.BLACK)
@@ -13,6 +15,8 @@ def draw_flags():
     arcade.draw_triangle_filled(165, 220, 165, 200, 130, 210, arcade.color.RED)
     arcade.draw_triangle_filled(255, 220, 255, 200, 220, 210, arcade.color.RED)
     arcade.draw_triangle_filled(345, 220, 345, 200, 310, 210, arcade.color.RED)
+
+# Draws the sandcastle
 def draw_SandCastle():
     arcade.draw_rectangle_filled(260, 100, 250, 100, arcade.color.SANDY_TAUPE)
 
@@ -31,8 +35,12 @@ def draw_SandCastle():
     arcade.draw_rectangle_filled(330, 180, 10, 15, arcade.color.SANDY_TAUPE)
     arcade.draw_rectangle_filled(349, 180, 10, 15, arcade.color.SANDY_TAUPE)
     arcade.draw_rectangle_filled(368, 180, 10, 15, arcade.color.SANDY_TAUPE)
+
+# Draw the sun
 def draw_sun():
     arcade.draw_circle_filled(600, 500, 70, arcade.color.YELLOW)
+
+# Draw birds
 def draw_birds():
     arcade.draw_arc_outline(200, 500, 90, 100, arcade.color.BLACK, 0, 90)
     arcade.draw_arc_outline(290, 500, -90, 100, arcade.color.BLACK, 0, 90)
@@ -44,12 +52,15 @@ def draw_birds():
 
     arcade.draw_arc_outline(300, 440, 90, 100, arcade.color.BLACK, 0, 100)
     arcade.draw_arc_outline(390, 440, -90, 100, arcade.color.BLACK, 0, 100)
+
+# Draw the sharks
 def draw_shark(x, y):
     arcade.draw_point(x,y+50,arcade.color.BLACK,5)
 
     arcade.draw_arc_filled(x, 60 + y, 90, 80, arcade.color.ALICE_BLUE, 0, 150)
     arcade.draw_circle_filled(x - 9, 72 + y, 31, arcade.color.BLUE_SAPPHIRE)
 
+# Set the animation of the shark/s
 def on_draw(delta_time):
     arcade.start_render()
     draw_sea()
@@ -65,15 +76,11 @@ def on_draw(delta_time):
     on_draw.shark1_y += 0.5
     on_draw.shark1_z += 2
 
-
 on_draw.shark1_x = 80
 on_draw.shark1_y = 80
 on_draw.shark1_z = 80
 
-
-
-
-
+# Set main function
 def main():
     arcade.open_window(800, 600, "praia de areia", resizable=True, antialiasing=True)
     arcade.set_background_color(arcade.color.BLUE_YONDER)
@@ -81,7 +88,6 @@ def main():
     arcade.schedule(on_draw, 1 / 60)
 
     arcade.run()
-
 
 # Call the main function
 main()
